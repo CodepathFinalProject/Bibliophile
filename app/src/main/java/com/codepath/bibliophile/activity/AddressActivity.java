eec018b56785ff6df6a39e0d41fc2070afef5663
Delnaz@ZED-DEV <ZED-DEV@192.168.10.164>
2016-08-27 15:29:40 -0700

    store data in parse user and profile fragment retrieving / saving user info

--- app/src/main/java/com/codepath/bibliophile/activity/AddressActivity.java
+++ app/src/main/java/com/codepath/bibliophile/activity/AddressActivity.java
@@ -3,11 +3,19 @@ package com.codepath.bibliophile.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.codepath.bibliophile.R;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +25,6 @@ import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getAddressL
 import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getCity;
 import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getState;
 import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getZipcode;
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.setAddress;
 
 public class AddressActivity extends AppCompatActivity {
 
@@ -73,10 +80,46 @@ public class AddressActivity extends AppCompatActivity {
         etZipcode.setText(etZipcode.getText());
         zipcode = etZipcode.getText().toString();
 
+        saveToParse();
 
-        setAddress(getApplicationContext(),addre1,addre2,city,state,zipcode);
-        Toast.makeText(AddressActivity.this, "shared preference result : " + getAddressLine1(getApplicationContext()) + " :: " + getAddressLine2(getApplicationContext()) + " :: "  + getCity(getApplicationContext())+ " :: " + getState(getApplicationContext()) + " :: " + getZipcode(getApplicationContext()), Toast.LENGTH_SHORT).show();
+      //  setAddress(getApplicationContext(),addre1,addre2,city,state,zipcode);
+       // Toast.makeText(AddressActivity.this, "shared preference result : " + getAddressLine1(getApplicationContext()) + " :: " + getAddressLine2(getApplicationContext()) + " :: "  + getCity(getApplicationContext())+ " :: " + getState(getApplicationContext()) + " :: " + getZipcode(getApplicationContext()), Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(AddressActivity.this,HomeMainActivity.class);
         startActivity(intent);
     }
+
+
+
+    private void saveToParse() {
+        final Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,picture.type(large)");
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me", parameters, HttpMethod.GET, new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(final GraphResponse response) {
+                if(response != null){
+                    try {
+                        JSONObject data = response.getJSONObject();
+                        String profilePicUrl = "";
+                        if (data.has("picture")) {
+                            profilePicUrl = data.getJSONObject("picture").getJSONObject("data").getString("url");
+                        }
+                        Log.d("3764", "onCompleted: " + response.getJSONObject().toString());
+                        String email = response.getJSONObject().getString("email");
+                        String name = response.getJSONObject().getString("name");
+                        String address = getAddressLine1(getApplicationContext()) + " " + getAddressLine2(getApplicationContext()) + ", " + getCity(getApplicationContext()) + ", " + getState(getApplicationContext()) + " - " + getZipcode(getApplicationContext());
+
+                        ParseUser parseUser = ParseUser.getCurrentUser();
+                        parseUser.setEmail(email);
+                        parseUser.setUsername(name);
+                        parseUser.put("address",address);
+                        parseUser.put("profilePic",profilePicUrl);
+                        parseUser.saveEventually();
+                    } catch (final JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).executeAsync();
+    }
+
 }
--- app/src/main/java/com/codepath/bibliophile/fragment/ProfileFragment.java
+++ app/src/main/java/com/codepath/bibliophile/fragment/ProfileFragment.java
@@ -2,23 +2,25 @@ package com.codepath.bibliophile.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.codepath.bibliophile.R;
-
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getAddressLine1;
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getAddressLine2;
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getCity;
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getState;
-import static com.codepath.bibliophile.utils.SharedPreferenceUtility.getZipcode;
+import com.parse.ParseUser;
 
 public class ProfileFragment extends BaseFragment {
 
     EditText etAddress;
+    EditText etName;
+    ImageView ivProfilePic;
+    EditText etEmail;
+    Button btnSaveProfile;
 
     public ProfileFragment(){
 
@@ -35,8 +37,36 @@ public class ProfileFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        etName = (EditText) view.findViewById(R.id.etName);
+        etName.setText(ParseUser.getCurrentUser().getString("username"));
+        etName.setSelection(etName.length());
+        etEmail = (EditText) view.findViewById(R.id.etEmail);
+        etEmail.setText(ParseUser.getCurrentUser().getString("email"));
+        etEmail.setSelection(etEmail.length());
         etAddress = (EditText) view.findViewById(R.id.etAddress);
-        etAddress.setText(getAddressLine1(getContext()) + " " + getAddressLine2(getContext()) +", " + getCity(getContext()) + ", " + getState(getContext()) + " - " + getZipcode(getContext()));
+        etAddress.setText(ParseUser.getCurrentUser().getString("address"));
         etAddress.setSelection(etAddress.length());
+        btnSaveProfile = (Button) view.findViewById(R.id.btnSaveProfile);
+
+        btnSaveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser parseUser = ParseUser.getCurrentUser();
+                parseUser.setEmail(etEmail.getText().toString());
+                parseUser.setUsername(etName.getText().toString());
+                parseUser.put("address",etAddress.getText().toString());
+                parseUser.saveEventually();
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Profile Saved Succesfully!")
+                        .setCancelable(true);
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+
     }
 }
