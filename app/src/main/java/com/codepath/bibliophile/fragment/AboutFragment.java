4a8a51b229ce1c4f42ca050cea9c6d6f4f6d7319
supriyapremkumar <spremkum@ucsc.edu>
2016-09-07 14:28:00 -0700

    About page clean up and privacy notices

--- app/src/main/java/com/codepath/bibliophile/fragment/AboutFragment.java
+++ app/src/main/java/com/codepath/bibliophile/fragment/AboutFragment.java
@@ -6,13 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.codepath.bibliophile.R;
 
 public class AboutFragment extends Fragment {
-
-    TextView tvAbout;
     public AboutFragment() {
     }
 
@@ -20,12 +17,6 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_about, parent, false);
-
-        tvAbout = (TextView) v.findViewById(R.id.tvAbout);
-
-        // add the about message into the tvAbout textview
-        String data = "Bibliophile is a mobile-first peer to peer app, where we provide a fully functional marketplace heavily optimized for buying and selling books.";
-        tvAbout.setText(data);
         return v;
     }
 }
